725bcfdc629bccc84532130a5fb3f206206699da
Macy Corbin <macycorbin@Macys-MacBook-Pro.local>
2019-05-20 10:08:49 -0400

    updated database, created product list function and table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,12 +13,30 @@ connection.connect(function(err){
     if(err) throw err;
     console.log("connected as id " + connection.threadId);
 
-    selectAll();
 })
 
-function selectAll(){
-    connection.query("SELECT * FROM products", function(err, res){
-        if (err) throw err;
-        console.log(res);
-    })
+//This is the list of products
+var productList = function(){
+    var query = "SELECT * FROM products";
+    connection.query(query,function(err,res){
+        if(err) throw err;
+        var displayList = new Table({
+            head:["Item ID", "Product Name", "Catergory", "Price", "Quantity"],
+            colWidths: [15, 30, 30, 15, 15]
+        });
+
+        for (var i = 0; i< res.length; i++){
+            displayList.push(
+                [res[i].product_id, res[i].product_name, res[i].department_name, res[i].price, res[i].quantity]
+            );
+        }
+
+        console.log(displayList.toString());
+        purchaseProduct();
+
+    });
 }
+
+
+//The inquirer prompt guides the user for purchasing a product by asking for the ID and the Quantity
+//function purchaseProduct()
